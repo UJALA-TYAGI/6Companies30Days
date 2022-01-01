87dd03623fc62b9a4e52b668fd42aece928869fa
UJALA TYAGI <UJALATYAGI2512@GMAIL.COM>
2022-01-01 19:01:18 +0530

    Add files via upload

---
+++ GoldmanSachs/Q1.cpp
@@ -0,0 +1,78 @@
+// { Driver Code Starts
+//Initial Template for C++
+#include <bits/stdc++.h>
+#include <unordered_map>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+  public:
+    vector<vector<string> > Anagrams(vector<string>& string_list) {
+        //code here
+        map<map<char, int>, vector<string>> v;
+
+        for(string s : string_list)
+        {
+
+            map<char, int> mp;
+            vector<string> temp;
+            for(int i = 0; i < s.length(); ++i)
+            {
+                ++mp[s[i]];
+            }
+
+            auto it = v.find(mp);
+            if (it != v.end())
+            {
+                it->second.push_back(s);
+            }
+            else
+            {
+                temp.push_back(s);
+                v.insert({mp, temp});
+            }
+        }
+
+        vector<vector<string>> res;
+
+        for(auto it = v.begin();it != v.end(); ++it)
+        {
+            res.push_back(it->second);
+        }
+
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<string> string_list(n);
+        for (int i = 0; i < n; ++i)
+            cin>>string_list[i];
+        Solution ob;
+        vector<vector<string> > result = ob.Anagrams(string_list);
+        sort(result.begin(),result.end());
+        for (int i = 0; i < result.size(); i++)
+        {
+            for(int j=0; j < result[i].size(); j++)
+            {
+                cout<<result[i][j]<<" ";
+            }
+            cout<<"\n";
+        }
+    }
+
+    return 0;
+}
+  // } Driver Code Ends
