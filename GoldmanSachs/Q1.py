d9ac7175262f6b1d4417c961282ca3d5747d88bd
UJALA TYAGI <UJALATYAGI2512@GMAIL.COM>
2022-01-01 19:03:57 +0530

    Create Q1.py

---
+++ GoldmanSachs/Q1.py
@@ -0,0 +1,48 @@
+collections import defaultdict
+
+class Solution:
+    def Anagrams(self, words, n):
+        '''
+        words: fromlist of word
+        n:      no of words
+        return : list of group of anagram {list will be sorted in driver code (not word in grp)}
+        '''
+        #         d=defaultdict(list)
+    
+        # for i,e in enumerate(s):
+        #     e=str(sorted(e))
+        #     d[e].append(s[i])
+    
+        # res=[]
+        # for l in d.values():
+        #     res.append(l)
+        # return res
+        #code here
+        anagrams = defaultdict(list)
+        for i in words:
+            anagrams[str(sorted(i))].append(i)
+            
+        result = []
+        for v in anagrams.values():
+            result.append(v)
+        return result
+      
+  #  Driver Code Starts
+  #Initial Template for Python 3
+
+  #contributed by RavinderSinghPB
+  if __name__ =='__main__':
+      t= int(input())
+      for tcs in range(t):
+          n= int(input())
+          words=input().split()
+
+          ob = Solution()
+          ans = ob.Anagrams(words,n)
+
+          for grp in sorted(ans):
+              for word in grp:
+                  print(word,end=' ')
+              print()
+
+  # } Driver Code Ends
