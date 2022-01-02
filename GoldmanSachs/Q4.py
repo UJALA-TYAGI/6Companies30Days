fbf781a0ead0da7d1c6c0e24a3da441217f4f442
UJALA TYAGI <UJALATYAGI2512@GMAIL.COM>
2022-01-02 23:37:37 +0530

    Update Q4.py

--- GoldmanSachs/Q4.py
+++ GoldmanSachs/Q4.py
@@ -1,47 +1,32 @@
-#User function Template for python3
-class Solution:
-    def getNthUglyNo(self,n):
-        # code here
-        ugly = [0] * n
-        ugly[0] = 1
-     
-        i2 = i3 = i5 = 0
-     
-        mul_of_2 = 2
-        mul_of_3 = 3
-        mul_of_5 = 5
-
-        for i in range(1, n):
-            
-            ugly[i] = min(mul_of_2, mul_of_3, mul_of_5)
-     
-            if ugly[i] == mul_of_2:
-                i2 += 1
-                mul_of_2 = ugly[i2] * 2
-     
-            if ugly[i] == mul_of_3:
-                i3 += 1
-                mul_of_3 = ugly[i3] * 3
-     
-            if ugly[i] == mul_of_5:
-                i5 += 1
-                mul_of_5 = ugly[i5] * 5
-     
-        return ugly[-1]
+#Your task is to complete this function
+#Function should return complete string
+from collections import OrderedDict
+def encode(arr):
+    # Code here
+    comp= []
+    count = 1
+    c = arr[0]
+    for i in range(1,len(arr)):
+        if arr[i] == c:
+            count = count + 1
+        else :
+            comp.append([c,count])
+            c = arr[i]
+            count = 1
+    comp.append([c,count])
+    s = ''
+ 
+    for i in range(0,len(comp)):
+        for j in comp[i]:
+            s += str(j)
+    return s
 
 #{ 
 #  Driver Code Starts
-#Initial Template for Python 3
-
-
-
-if __name__ == '__main__':
-    tc = int(input())
-    while tc > 0:
-        n = int(input())
-        ob = Solution()
-        ans = ob.getNthUglyNo(n);
-        print(ans)
-        tc -= 1
-
+#Your code will prepended here
+if __name__=='__main__':
+    t=int(input())
+    for i in range(t):
+        arr=input().strip()
+        print (encode(arr))
 # } Driver Code Ends
