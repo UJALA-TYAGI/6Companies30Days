230bd104e203ef4fd2f2a0c367e01bca4895f5d5
UJALA TYAGI <UJALATYAGI2512@GMAIL.COM>
2022-01-05 00:06:08 +0530

    Create Q12.cpp

---
+++ GoldmanSachs/Q12.cpp
@@ -0,0 +1,31 @@
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+    long long squaresInChessBoard(long long n) {
+        // code here
+        long long total = (n*(n+1)*(2*n +1))/6;
+         
+        return total;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        long long N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.squaresInChessBoard(N) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
