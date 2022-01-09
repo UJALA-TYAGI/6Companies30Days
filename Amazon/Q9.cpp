0f5be889fbe07f717fe7535db1594abd78101099
UJALA TYAGI <UJALATYAGI2512@GMAIL.COM>
2022-01-09 22:12:34 +0530

    Create Q9.cpp

---
+++ Amazon/Q9.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+// User function Template for C++
+
+class Solution{
+public:
+    int isValid(vector<vector<int>> board){
+        // code here
+         int used1[9][9] = {0}, used2[9][9] = {0}, used3[9][9] = {0};
+        
+        for(int i = 0; i < board.size(); ++ i)
+            for(int j = 0; j < board[i].size(); ++ j)
+                if(board[i][j] != 0)
+                {
+                    int num = board[i][j] - 1, k = i / 3 * 3 + j / 3;
+                    if(used1[i][num] || used2[j][num] || used3[k][num])
+                        return 0;
+                    used1[i][num] = used2[j][num] = used3[k][num] = 1;
+                }
+        
+        return 1;
+    }
+};
+
+// { Driver Code Starts.
+
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        vector<vector<int>> mat(9, vector<int>(9, 0));
+        for(int i = 0;i < 81;i++)
+            cin>>mat[i/9][i%9];
+        
+        Solution ob;
+        cout<<ob.isValid(mat)<<"\n";
+    }
+    return 0;
+}  // } Driver Code Ends
